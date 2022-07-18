b15b578570a925f03da696c090946c0a25fa56de
JohnBerglund <johndberglund@gmail.com>
2022-07-17 22:03:06 -0500

    changed rosette for large n to be nicer.

--- decor.js
+++ decor.js
@@ -670,9 +670,15 @@ function SVGrosettes() {
       var ratio = Math.sin(Math.PI/n)/Math.sin(Math.PI/n+nRoseAngle/2);
       var B = ratio*Math.sin(Math.PI/2-nRoseAngle/2);
       var A = ratio*Math.sin(Math.PI/2-Math.PI/n);
-      var X = H*A/(H+A);
-      var C = X*(1-B)/A;
-      var D = C/2/H;
+      if (n>12) {
+        var X = A*.6;
+        var D = H/3;
+        var C = (1-B)*2/3;
+      } else {
+        var X = H*A/(H+A);
+        var C = X*(1-B)/A;
+        var D = C/2/H;
+      }
 
       poly.forEach(function(point) {     
         var Pt1 = avePtMap([point, lastPt]);
@@ -1014,7 +1020,7 @@ function drawStars(context) {
   });
 } /* end drawStars */
 
-function drawRosettes(context) {
+function drawRosettesA(context) {
   tiles.polys.forEach(function(poly) {
     var myAngle = document.getElementById("demo").innerHTML;
     var nRoseAngle=Math.PI/180*myAngle;
@@ -1121,6 +1127,123 @@ function drawRosettes(context) {
     }; //end n >=5
   }); // end poly loop
 } /* end drawRosettes */
+
+
+
+function drawRosettes(context) {
+  tiles.polys.forEach(function(poly) {
+    var myAngle = document.getElementById("demo").innerHTML;
+    var nRoseAngle=Math.PI/180*myAngle;
+    var center = avePtMap(poly);
+    var n = poly.length;
+    var lastPt = poly[n-1];
+    if (n<5) {
+      var newPoly = [];
+      var R = 1/(2*Math.sin(Math.PI/n));
+      var H = 1/(2*Math.tan(Math.PI/n));
+      var vertWt = H*Math.sin(nRoseAngle/2)/Math.sin(Math.PI-nRoseAngle/2-Math.PI/n);
+      var centWt = R - vertWt;
+      poly.forEach(function(point) {     
+        var midPt = avePtMap([point, lastPt]);
+        newPoly.push(midPt);
+        var rawPoint = mapping(tiles.pts[point[0]],point[1]);
+        var newPt = weightPts(center,centWt,rawPoint,vertWt);
+        newPoly.push(newPt);
+        lastPt = point;      
+      });
+
+      for (i = -2;i<5;i++) {
+        for (j = -2;j<5;j++) {
+          context.beginPath();
+          context.strokeStyle ="black";
+     //     context.fillStyle = "purple";
+          var red = 80*(1+poly.length-3*Math.round(poly.length/3));
+          var green = 50*(1+poly.length-5*Math.round(poly.length/5));
+          var blue = 30*(1+poly.length-7*Math.round(poly.length/7));
+          context.fillStyle = "rgb("+red+","+green+","+blue+")";
+          var newPoint = newPoly[0];
+          context.moveTo(
+           (newPoint[0]+200+i*Ax+j*Bx)*sized,
+           (newPoint[1]+15+i*Ay+j*By)*sized
+          );
+          newPoly.forEach(function(point) {
+            context.lineTo(
+             (point[0]+200+i*Ax+j*Bx)*sized,
+             (point[1]+15+i*Ay+j*By)*sized
+            );	
+          });
+          context.closePath();
+          context.fill();
+      //    context.stroke();
+        } /* end j loop */
+      } /* end i loop */
+    } //end n < 5
+    else {
+      var lastRawPt = mapping(tiles.pts[lastPt[0]],lastPt[1]);
+      var H = Math.cos(Math.PI/n);
+      var ratio = Math.sin(Math.PI/n)/Math.sin(Math.PI/n+nRoseAngle/2);
+      var B = ratio*Math.sin(Math.PI/2-nRoseAngle/2);
+      var A = ratio*Math.sin(Math.PI/2-Math.PI/n);
+      if (n>12) {
+          var X = A*.6;
+          var D = H/3;
+          var C = (1-B)*2/3;
+      }
+      else {
+        var X = H*A/(H+A);
+        var C = X*(1-B)/A;
+        var D = C/2/H;
+      }
+      poly.forEach(function(point) {     
+        var Pt1 = avePtMap([point, lastPt]);
+        var petals = [];
+        petals.push(Pt1);
+        var rawPt = mapping(tiles.pts[point[0]],point[1]);
+        var tempPt1 = weightPts(center,B,rawPt,1-B);
+        var Pt2 = weightPts(tempPt1,X,Pt1,A-X);
+        petals.push(Pt2);
+        var Pt3 = weightPts(rawPt,C,center,1-C);
+        petals.push(Pt3);   
+        var Pt4 = weightPts(Pt1,D,center,H-D);
+        petals.push(Pt4);
+        var Pt5 = weightPts(lastRawPt,C,center,1-C);
+        petals.push(Pt5);
+        var tempPt2 = weightPts(center,B,lastRawPt,1-B);
+        var Pt6 = weightPts(tempPt2,X,Pt1,A-X);
+        petals.push(Pt6);
+        lastPt = point;  
+        lastRawPt = rawPt;  
+  
+        for (i = -2;i<5;i++) {
+          for (j = -2;j<5;j++) {
+            context.beginPath();
+            context.strokeStyle ="black";
+        //    context.fillStyle = "purple";
+            var red = 80*(1+poly.length-3*Math.round(poly.length/3));
+            var green = 50*(1+poly.length-5*Math.round(poly.length/5));
+            var blue = 30*(1+poly.length-7*Math.round(poly.length/7));
+            context.fillStyle = "rgb("+red+","+green+","+blue+")";
+            var newPoint = petals[0];
+            context.moveTo(
+             (newPoint[0]+200+i*Ax+j*Bx)*sized,
+             (newPoint[1]+15+i*Ay+j*By)*sized
+            );
+            petals.forEach(function(point) {
+              context.lineTo(
+               (point[0]+200+i*Ax+j*Bx)*sized,
+               (point[1]+15+i*Ay+j*By)*sized
+              );	
+            });
+            context.closePath();
+            context.fill();
+        //    context.stroke();
+          } /* end j loop */
+        } /* end i loop */
+      }); // end point loop
+    }; //end n >=5
+  }); // end poly loop
+} /* end drawRosettes */
+
 
 function drawExtRoses(context) {
   tiles.polys.forEach(function(poly) {
